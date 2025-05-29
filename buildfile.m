45d51e486dfc86c12384d173a06ae55157361fe8
Michelle Hirsch <mhirsch@mathworks.com>
2025-05-29 16:44:03 -0400

    Refactor CI workflow to use standardized build action and update test reporting

--- buildfile.m
+++ buildfile.m
@@ -4,8 +4,10 @@ function plan = buildfile
     % Task for linting
     plan("lint") = matlab.buildtool.tasks.CodeIssuesTask(["+geometry", "examples", "tests"]); 
 
-    % Task for running tests
-    plan("test") = matlab.buildtool.tasks.TestTask("tests"); % Specify source folder for test discovery
+    % Task for running tests and generating a JUnit XML report
+    plan("test") = matlab.buildtool.tasks.TestTask("tests", ... 
+        TestResults = "test-results/results.xml"); % Specify output file for JUnit XML
+
     plan("test").Dependencies = "lint"; % Make test depend on lint passing
 
     plan.DefaultTasks = ["lint", "test"];
